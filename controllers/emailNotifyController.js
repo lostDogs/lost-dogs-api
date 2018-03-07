2542671da074775cfcc1a51ddf9545a0f2530adf
Chris Zepeda <chris.zeag@gmail.com>
2018-03-07 14:07:04 -0600

    test for bounce

--- controllers/emailNotifyController.js
+++ controllers/emailNotifyController.js
@@ -6,6 +6,10 @@ module.exports = () => {
 const bounces = (req, res) => {
   console.log('bounces subscription headers >> ', req.headers);
   console.log('bounces subscription body >> ', req.body);
+  console.log('<<<<<<< end bounce subscription body  >>>>>> ',);
+  const message = JSON.parse(JSON.parse(req.body).Message);
+  console.log('bounces  message >> ', message);
+  console.log('bounces  emailAddress >> ', message.bounce.bouncedRecipients[0].emailAddress);
   return res.status(200).json();
   };
 
